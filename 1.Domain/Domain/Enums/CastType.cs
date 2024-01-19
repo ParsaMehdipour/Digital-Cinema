06130a1375dafe2d3601e7703e947fbdb8c0080b
p.mehdipour <mehdipourparsa@gmail.com>
2024-01-19 19:03:32 +0330

    feat : add CastType enum

---
+++ 1.Domain/Domain/Enums/CastType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums;
+
+public enum CastType
+{
+    [Display(Name = "Actor/Actress")]
+    ActorActress = 0,
+
+    [Display(Name = "Director")]
+    Director = 1,
+
+    [Display(Name = "Writer")]
+    Writer = 2
+}
