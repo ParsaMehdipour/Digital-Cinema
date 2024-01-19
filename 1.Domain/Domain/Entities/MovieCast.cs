0a9617a82e22b9c8d6cf7b1c19ac42c82373e2c3
p.mehdipour <mehdipourparsa@gmail.com>
2024-01-19 19:04:38 +0330

    feat : add MovieCast entity

---
+++ 1.Domain/Domain/Entities/MovieCast.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enums;
+using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public class MovieCast : Entity
+{
+    #region Fields
+
+    public Guid CastId { get; private set; }
+    public Guid MovieId { get; private set; }
+    public CastType CastType { get; private set; }
+
+    #endregion
+
+    #region Ctor
+
+    private MovieCast(Guid id, Guid castId, Guid movieId, CastType castType) : base(id)
+    {
+        SetCastId(castId);
+        SetMovieId(movieId);
+        SetCastType(castType);
+    }
+
+    private MovieCast()
+    {
+
+    }
+
+    #endregion
+
+    #region Methods
+
+    public MovieCast Create(Guid id, Guid castId, Guid movieId, CastType castType) => new(id, castId, movieId, castType);
+
+    public void SetCastId(Guid castId) => CastId = castId;
+
+    public void SetMovieId(Guid movieId) => MovieId = movieId;
+
+    public void SetCastType(CastType castType) => CastType = castType;
+
+    #endregion
+}
