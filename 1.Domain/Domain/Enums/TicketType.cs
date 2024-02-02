b6c75811c5729c650a466c1ac7c56b2eb15643a3
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 20:33:45 +0330

    feat : add TicketType enum

---
+++ 1.Domain/Domain/Enums/TicketType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums;
+public enum TicketType
+{
+    [Display(Name = "Online")]
+    Online = 0,
+
+    [Display(Name = "Onsite")]
+    Onsite = 1
+}
