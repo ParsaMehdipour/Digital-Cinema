7a459247c5a5a5ee6edb1c0570be0ed923f69839
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 20:33:08 +0330

    feat : add entites
    1.Cinema
    2;Hall
    3.MoviePlay
    4.State
    5.Ticket

---
+++ 1.Domain/Domain/Entities/State.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Primitives;
+using Domain.ValueObjects;
+
+namespace Domain.Entities;
+public class State : Entity
+{
+
+    #region Fields
+
+    public StateName StateName { get; private set; } = null!;
+
+    #endregion
+
+    #region Ctor
+
+    private State(StateName stateName) => StateName = stateName;
+
+    #endregion
+
+    #region Methods
+
+    public State Create(StateName stateName) => new(stateName);
+
+    public void SetStateName(StateName stateName)
+    {
+        if (StateName.Equals(stateName)) return;
+
+        StateName = stateName;
+    }
+
+    #endregion
+}
+
---
+++ 1.Domain/Domain/Entities/Ticket.cs
@@ -0,0 +1,132 @@
+﻿using Domain.Enums;
+using Domain.Primitives;
+
+namespace Domain.Entities;
+public class Ticket : Entity
+{
+    #region Fields
+
+    public Guid MovieId { get; private set; }
+
+    public Guid CustomerId { get; private set; }
+
+    public Guid HallId { get; private set; }
+
+    public TicketType Type { get; private set; }
+
+    public DateTime DateAndTime { get; private set; }
+
+    public bool Sold { get; private set; }
+
+    public bool Intended { get; private set; }
+
+    public int SeatNumber { get; private set; }
+
+    public string ReferenceNumber { get; private set; } = string.Empty;
+
+    #endregion
+
+    #region Ctor
+
+    private Ticket() { }
+
+    private Ticket(Guid movieId, Guid customerId, Guid hallId, TicketType type, DateTime dateAndTime, bool sold, bool intended, int seatNumber, string referenceNumber)
+    {
+        SetMovieId(movieId);
+        SetCustomerId(customerId);
+        SetHallId(hallId);
+        SetTicketType(type);
+        SetDateAndTime(dateAndTime);
+        SetSold(true);
+        SetIntended(false);
+        SetSeatNumber(seatNumber);
+        SetReferenceNumber(referenceNumber);
+    }
+
+    #endregion
+
+    #region Methods
+
+    public Ticket Create(Guid movieId, Guid customerId, Guid hallId, TicketType type, DateTime dateAndTime, bool sold, bool intended, int seatNumber, string referenceNumber) => new(movieId,
+        customerId,
+        hallId,
+        type,
+        dateAndTime,
+        sold,
+        intended,
+        seatNumber,
+        referenceNumber);
+
+    public void SetMovieId(Guid movieId)
+    {
+        if (movieId == Guid.Empty) return;
+
+        if (MovieId.Equals(movieId)) return;
+
+        MovieId = movieId;
+    }
+
+    public void SetCustomerId(Guid customerId)
+    {
+        if (customerId == Guid.Empty) return;
+
+        if (CustomerId.Equals(customerId)) return;
+
+        CustomerId = customerId;
+    }
+
+    public void SetHallId(Guid hallId)
+    {
+        if (hallId == Guid.Empty) return;
+
+        if (hallId.Equals(hallId)) return;
+
+        HallId = hallId;
+    }
+
+    public void SetTicketType(TicketType type)
+    {
+        if (Type.Equals(type)) return;
+
+        Type = type;
+    }
+
+    public void SetDateAndTime(DateTime dateAndTime)
+    {
+        if (DateAndTime.Equals(dateAndTime)) return;
+
+        DateAndTime = dateAndTime;
+    }
+
+    public void SetSold(bool sold)
+    {
+        if (Sold.Equals(sold)) return;
+
+        Sold = sold;
+    }
+
+    public void SetIntended(bool intended)
+    {
+        if (Intended.Equals(intended)) return;
+
+        Intended = intended;
+    }
+
+    public void SetSeatNumber(int seatNumber)
+    {
+        if (SeatNumber.Equals(seatNumber)) return;
+
+        SeatNumber = seatNumber;
+    }
+
+    public void SetReferenceNumber(string refereneNumber)
+    {
+        if (string.IsNullOrEmpty(refereneNumber)) return;
+
+        if (ReferenceNumber.Equals(refereneNumber)) return;
+
+        ReferenceNumber = refereneNumber;
+    }
+
+    #endregion
+}
