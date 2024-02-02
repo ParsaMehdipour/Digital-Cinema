c65427a48523471a8b6daabd2bd3bc1829105ddd
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 18:16:52 +0330

    refactor : change DomainEvent
    1.Make it a class
    2.Add fileds : ( Id , Data , UserId )

--- 1.Domain/Domain/DomainEvents/DomainEvent.cs
+++ 1.Domain/Domain/DomainEvents/DomainEvent.cs
@@ -2,5 +2,20 @@
 
 namespace Domain.DomainEvents;
 
-public abstract record DomainEvent(Guid Id) : IDomainEvent;
+public class DomainEvent : IDomainEvent
+{
+    #region Fields
+
+    public Guid Id { get; set; }
+    public string Data { get; set; } = string.Empty;
+    public Guid UserId { get; set; } = Guid.Empty;
+
+    #endregion
+
+    #region Ctor
+
+    public DomainEvent() { }
+
+    #endregion
+}
 
