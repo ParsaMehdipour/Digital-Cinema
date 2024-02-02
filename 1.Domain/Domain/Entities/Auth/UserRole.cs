a6f4519a81ceb563d8d79ff31997d990225382c9
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 18:27:53 +0330

    feat : add UserRole

---
+++ 1.Domain/Domain/Entities/Auth/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Primitives;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities.Auth;
+public class UserRole : IdentityUserRole<Guid>, IAuditableEntity
+{
+    public DateTime CreatedOnUtc { get; set; }
+    public DateTime? ModifiedOnUtc { get; set; }
+    public bool IsDeleted { get; set; }
+}
