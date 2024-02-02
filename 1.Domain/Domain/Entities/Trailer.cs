1eef28a1fcb6023c5693c15509f58b527d980963
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 19:20:09 +0330

    feat : add new entites
    1.trailer
    2.City

---
+++ 1.Domain/Domain/Entities/Trailer.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Primitives;
+using Domain.ValueObjects;
+
+namespace Domain.Entities;
+public class Trailer : Entity
+{
+    #region Fields
+
+    public Guid MovieId { get; private set; }
+
+    public TrailerDurationInMinutes TrailerDurationInMinutes { get; private set; } = null!;
+
+    public DateTime ReleaseDate { get; private set; }
+
+    public string FilePath { get; private set; } = string.Empty;
+
+
+    #endregion
+
+    #region Methods
+
+    public void SetMovieId(Guid movieId)
+    {
+        if (MovieId.Equals(movieId)) return;
+
+        if (movieId == Guid.Empty) return;
+
+        MovieId = movieId;
+    }
+
+    public void SetTrailerDurationInMinutes(TrailerDurationInMinutes trailerDurationInMinutes)
+    {
+        if (TrailerDurationInMinutes.Equals(trailerDurationInMinutes)) return;
+
+        TrailerDurationInMinutes = trailerDurationInMinutes;
+    }
+
+    public void SetReleaseDate(DateTime releaseDate)
+    {
+        if (ReleaseDate.Equals(releaseDate)) return;
+
+        ReleaseDate = releaseDate;
+    }
+
+    public void SetFilePath(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath)) return;
+
+        if (FilePath.Equals(filePath)) return;
+
+        FilePath = filePath;
+    }
+
+    #endregion
+}
