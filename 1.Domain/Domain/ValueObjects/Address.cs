dbf18abf053ac2b9cf701eb84569b532d26dceb1
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 19:17:41 +0330

    feat : add value objects 1.Address 2.CinemaName 3.CitiyName 4.trailerDurationInMinutes

---
+++ 1.Domain/Domain/ValueObjects/Address.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Errors;
+using Domain.Primitives;
+using Domain.Shared;
+
+namespace Domain.ValueObjects;
+public sealed class Address : ValueObject
+{
+    private const int MaxLength = 1000;
+
+    private Address(string value) => Value = value;
+
+    public string Value { get; }
+
+    public Result<Address> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result.Failure<Address>(DomainErrors.Address.Empty);
+
+        if (value.Length > MaxLength)
+            return Result.Failure<Address>(DomainErrors.Address.TooLong);
+
+        return new Address(value);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
---
+++ 1.Domain/Domain/ValueObjects/TrailerDurationInMinutes.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Errors;
+using Domain.Primitives;
+using Domain.Shared;
+
+namespace Domain.ValueObjects;
+public sealed class TrailerDurationInMinutes : ValueObject
+{
+    private const int MaxDuration = 5;
+
+    private TrailerDurationInMinutes(int value) => Value = value;
+
+    public int Value { get; }
+
+    public Result<TrailerDurationInMinutes> Create(int value)
+    {
+        if (value < 0)
+            return Result.Failure<TrailerDurationInMinutes>(DomainErrors.TrailerDurationInMinutes.Negative);
+
+        if (value == 0)
+            return Result.Failure<TrailerDurationInMinutes>(DomainErrors.TrailerDurationInMinutes.Zero);
+
+        if (value > MaxDuration)
+            return Result.Failure<TrailerDurationInMinutes>(DomainErrors.TrailerDurationInMinutes.TooLong);
+
+        return new TrailerDurationInMinutes(value);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
