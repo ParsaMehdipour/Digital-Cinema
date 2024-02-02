6f295c999dc33e87e60497d719a4ae77447cc10c
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-02 18:17:49 +0330

    feat : add MovieUpdatedDomainEvent

---
+++ 1.Domain/Domain/DomainEvents/MovieUpdatedDomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+
+namespace Domain.DomainEvents;
+
+public class MovieUpdatedDomainEvent : DomainEvent
+{
+
+    #region Fields
+
+    public Movie Movie { get; init; }
+
+    #endregion
+
+    #region Ctor
+
+    public MovieUpdatedDomainEvent(Movie movie) => Movie = movie;
+
+    #endregion
+
+}
