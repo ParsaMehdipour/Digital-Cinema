ac3c39540d6ab0d0418509717e11009b30be3efd
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-03 21:04:35 +0330

    feat : add base repositories
    1.IQUeryRepository
    2.IRepository

---
+++ 1.Domain/Domain/Repositories/BaseReposotries/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Repositories.BaseReposotries;
+public interface IRepository<TEntity> : IQueryRepository<TEntity> where TEntity : Entity
+{
+    void Add(TEntity entity);
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+
+    void AddRange(IEnumerable<TEntity> entities);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+
+    void Update(TEntity entity);
+    void UpdateRange(List<TEntity> entities);
+
+    void Delete(TEntity entity);
+}
