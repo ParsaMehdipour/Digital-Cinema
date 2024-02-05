03cc6e025cd89f9832682d0fc0ac68cceac92176
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-05 23:43:23 +0330

    feat : add TableNames

---
+++ 3.Persistence/Persistence/Constants/TableNames.cs
@@ -0,0 +1,28 @@
+﻿namespace Persistence.Constants;
+internal static class TableNames
+{
+    internal const string Casts = "Casts";
+
+    internal const string Cinemas = "Cinemas";
+
+    internal const string Cities = "Cities";
+
+    internal const string Genres = "Genres";
+
+    internal const string Halls = "Halls";
+
+    internal const string Movies = "Movies";
+
+    internal const string MovieCasts = "MovieCasts";
+
+    internal const string MovieGenres = "MovieGenres";
+
+    internal const string MoviePlays = "MoviePlays";
+
+    internal const string States = "States";
+
+    internal const string Tickets = "Tickets";
+
+    internal const string Trailers = "Trailers";
+
+}
