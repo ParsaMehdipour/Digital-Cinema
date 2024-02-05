647ede84c2fba0b9746c9188fae76feb8d88430d
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-06 00:19:55 +0330

    feat : Add services to persistence layer
    1.Add ModelBuilderExtensions
    2.Add postgre ef core package

---
+++ 3.Persistence/Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Persistence.Extensions;
+public static class ModelBuilderExtensions
+{
+    /// <summary>
+    /// Set NEWSEQUENTIALID() sql function for all columns named "Id"
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    /// <param name="mustBeIdentity">Set to true if you want only "Identity" guid fields that named "Id"</param>
+    public static void AddSequentialGuidForIdConvention(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.AddDefaultValueSqlConvention("Id", typeof(Guid), "NEWSEQUENTIALID()");
+    }
+
+    /// <summary>
+    /// Set DefaultValueSql for specific property name and type
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    /// <param name="propertyName">Name of property wants to set DefaultValueSql for</param>
+    /// <param name="propertyType">Type of property wants to set DefaultValueSql for </param>
+    /// <param name="defaultValueSql">DefaultValueSql like "NEWSEQUENTIALID()"</param>
+    public static void AddDefaultValueSqlConvention(this ModelBuilder modelBuilder, string propertyName, Type propertyType, string defaultValueSql)
+    {
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            IMutableProperty property = entityType.GetProperties().SingleOrDefault(p => p.Name.Equals(propertyName, StringComparison.OrdinalIgnoreCase))!;
+            if (property.ClrType == propertyType)
+                property.SetDefaultValueSql(defaultValueSql);
+        }
+    }
+}
