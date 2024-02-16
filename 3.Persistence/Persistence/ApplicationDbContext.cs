14e41ecb17b1066c2ab764358298be8c424b79d9
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-16 15:14:18 +0330

    feat : add ApplicationDbContext

---
+++ 3.Persistence/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Repositories.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+
+/// <remarks>
+/// Add migrations using the following command inside the 'Persistence' project directory:
+///
+/// dotnet ef migrations add --startup-project WebApi --context ApplicationDdContext [migration-name]
+/// </remarks>
+public class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
