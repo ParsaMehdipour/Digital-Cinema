af1e8a98993b984e51f5d3f7b373725e92ee350b
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-19 01:08:07 +0330

    refactor : change domain event
    1.Add needed fields : TableName, TableRecordId

--- 1.Domain/Domain/DomainEvents/DomainEvent.cs
+++ 1.Domain/Domain/DomainEvents/DomainEvent.cs
@@ -7,6 +7,8 @@ public class DomainEvent : IDomainEvent
     #region Fields
 
     public Guid Id { get; set; }
+    public string TableName { get; set; } = string.Empty;
+    public string TableRecordId { get; set; } = string.Empty;
     public string Data { get; set; } = string.Empty;
     public Guid UserId { get; set; } = Guid.Empty;
 
