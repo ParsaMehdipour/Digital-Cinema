aed8097537fb5c3837254c786319686a548709f6
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-26 23:58:36 +0330

    feat : add CurrentUserService in charge of fetching the current user's id

---
+++ 0.SharedKernel/SharedKernel/UserServices/Services/CurrentUserService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using SharedKernel.UserServices.Interfaces;
+using System.Security.Claims;
+
+namespace SharedKernel.UserServices.Services;
+
+public class CurrentUserService(IHttpContextAccessor httpContextAccessor) : ICurrentUserService
+{
+    public Guid UserId => Guid.Parse(httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)!.Value ?? string.Empty);
+
+    public string UserIpAddress => httpContextAccessor.HttpContext?.Connection.RemoteIpAddress.ToString() ?? string.Empty;
+}
