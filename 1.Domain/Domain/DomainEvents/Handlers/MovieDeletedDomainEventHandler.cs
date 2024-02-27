ed92b772b0dc1363091e91f33d3da295c8a10b93
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-28 01:14:37 +0330

    feat : add domain event hanlders
    1.Created, Deleted, Restored, Updated handlers

---
+++ 1.Domain/Domain/DomainEvents/Handlers/MovieDeletedDomainEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities.Movies.Events;
+using Domain.Enums;
+using Domain.Repositories.DomainEvent;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SharedKernel.UserServices.Interfaces;
+
+namespace Domain.DomainEvents.Handlers;
+
+/// <summary>
+/// Handle's the event in which a movie is being deleted
+/// </summary>
+/// <param name="domainEventRepository"></param>
+/// <param name="logger"></param>
+/// <param name="currentUserService"></param>
+internal class MovieDeletedDomainEventHandler(IDomainEventRepository domainEventRepository, ILogger<MovieDeletedDomainEventHandler> logger, ICurrentUserService currentUserService) : INotificationHandler<MovieUpdatedDomainEvent>
+{
+    public async Task Handle(MovieUpdatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling domain event {state} ...", DomainEventState.Deleted.ToString());
+
+
+        DomainEvent domainEvent = new()
+        {
+            TableName = nameof(notification.Movie),
+            Data = "",
+            TableRecordId = notification.Movie.Id.ToString(),
+            State = DomainEventState.Deleted,
+            UserId = currentUserService.UserId
+        };
+
+        await domainEventRepository.InsertOneAsync(domainEvent);
+
+        logger.LogInformation("Domain event handled {id}", domainEvent.Id);
+    }
+}
---
+++ 1.Domain/Domain/DomainEvents/Handlers/MovieRestoredDomainEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities.Movies.Events;
+using Domain.Enums;
+using Domain.Repositories.DomainEvent;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SharedKernel.UserServices.Interfaces;
+
+namespace Domain.DomainEvents.Handlers;
+internal class MovieRestoredDomainEventHandler(IDomainEventRepository domainEventRepository, ILogger<MovieRestoredDomainEventHandler> logger, ICurrentUserService currentUserService)
+    : INotificationHandler<MovieRestoredDomainEvent>
+{
+    public async Task Handle(MovieRestoredDomainEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling domain event {state} ...", DomainEventState.Deleted.ToString());
+
+
+        DomainEvent domainEvent = new()
+        {
+            TableName = nameof(notification.Movie),
+            Data = "",
+            TableRecordId = notification.Movie.Id.ToString(),
+            State = DomainEventState.Deleted,
+            UserId = currentUserService.UserId
+        };
+
+        await domainEventRepository.InsertOneAsync(domainEvent);
+
+        logger.LogInformation("Domain event handled {id}", domainEvent.Id);
+    }
+}
