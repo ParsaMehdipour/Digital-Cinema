741c6bc6a4a6cae972535315e1d0d42f3bab8243
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-29 19:26:03 +0330

    refactor : change IQueryRepository and IRepository
    1.Add summaries

--- 1.Domain/Domain/Repositories/BaseRepositories/IQueryRepository.cs
+++ 1.Domain/Domain/Repositories/BaseRepositories/IQueryRepository.cs
@@ -4,16 +4,60 @@ using System.Linq.Expressions;
 namespace Domain.Repositories.BaseRepositories;
 public interface IQueryRepository<TEntity> where TEntity : Entity
 {
+    /// <summary>
+    /// List of <see cref="TEntity"/> by no tracking and filters by the given predicate
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
     IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+
+    /// <summary>
+    /// List of <see cref="TEntity"/> by no tracking
+    /// </summary>
+    /// <returns></returns>
     IQueryable<TEntity> Get();
 
+    /// <summary>
+    /// Finds item in <see cref="TEntity"/> by tracking.
+    /// </summary>
+    /// <returns></returns>
     TEntity GetById(params object[] ids);
+
+    /// <summary>
+    /// Finds async item in <see cref="TEntity"/> by tracking.
+    /// </summary>
+    /// <returns></returns>
     ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids);
+
+    /// <summary>
+    /// Ignores query filters
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task<TEntity> GetWithoutQueryFilterAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
 
-    Task<bool> IsExistsAsync(CancellationToken cancellationToken);
-    bool IsExists();
+    /// <summary>
+    /// Exists this <see cref="TEntity"/> in database by no tracking.
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> ExistsAsync(CancellationToken cancellationToken);
 
-    Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
-    bool IsExists(Expression<Func<TEntity, bool>> predicate);
+    /// <summary>
+    /// Exists async this <see cref="TEntity"/> in database by no tracking.
+    /// </summary>
+    /// <returns></returns>
+    bool Exists();
+
+    /// <summary>
+    /// Exists this <see cref="TEntity"/> in database by no tracking.
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Exists async this <see cref="TEntity"/> in database by no tracking.
+    /// </summary>
+    /// <returns></returns>
+    bool Exists(Expression<Func<TEntity, bool>> predicate);
 }
