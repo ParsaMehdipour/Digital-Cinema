ec152245787a05e88d1aa8e3e2aaad9c21fc3f53
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-29 19:22:28 +0330

    feat : add mongo db database settings classes

---
+++ 0.SharedKernel/SharedKernel/DataProviderSettings/MongoDB/BsonCollectionAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedKernel.DataProviderSettings.MongoDB;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class BsonCollectionAttribute : Attribute
+{
+    public string CollectionName { get; }
+
+    public BsonCollectionAttribute(string collectionName)
+    {
+        CollectionName = collectionName;
+    }
+}
---
+++ 0.SharedKernel/SharedKernel/DataProviderSettings/MongoDB/MongoDbDatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedKernel.DataProviderSettings.MongoDB;
+
+public class MongoDbDatabaseSettings
+{
+    public required string ConnectionString { get; set; }
+    public required string DatabaseName { get; set; }
+}
