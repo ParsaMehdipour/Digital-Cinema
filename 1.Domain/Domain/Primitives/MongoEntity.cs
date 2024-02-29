2e6af64ed7b0dd55fc3bcb923205ad33c2077cff
p.mehdipour <mehdipourparsa@gmail.com>
2024-02-29 19:24:40 +0330

    feat : add MongoEntity

---
+++ 1.Domain/Domain/Primitives/MongoEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Primitives;
+public class MongoEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
