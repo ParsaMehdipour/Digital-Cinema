c0f9f03055291e95be95bc33dffac5add5584de6
p.mehdipour <mehdipourparsa@gmail.com>
2024-04-05 15:56:55 +0330

    feat : add AutoMapper profile

---
+++ 2.Application/Application/MapperConfiguration/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Movies.DataTransferObjects;
+using AutoMapper;
+using Domain.Entities.Casts;
+
+namespace Application.MapperConfiguration;
+
+/// <summary>
+/// Represent an auto mapper profile
+/// </summary>
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Cast, CastDto>().ForMember(dest => dest.Age,
+                opt => opt.MapFrom(src => src.Age.Value))
+            .ForMember(dest => dest.FirstName,
+                opt => opt.MapFrom(src => src.FirstName.Value))
+            .ForMember(dest => dest.LastName,
+                opt => opt.MapFrom(src => src.LastName.Value));
+    }
+}
