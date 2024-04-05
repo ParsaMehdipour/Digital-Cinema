dfc972d77ffcdf0b1c80a421cf4d4264c568d340
p.mehdipour <mehdipourparsa@gmail.com>
2024-04-05 15:56:15 +0330

    feat : add EditCast command, validator and handler

---
+++ 2.Application/Application/Casts/Commands/EditCast/EditCastCommand.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+using FluentResults;
+using MediatR;
+
+namespace Application.Casts.Commands.EditCast;
+
+public record EditCastCommand(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    CastType CastType,
+    Gender Gender,
+    bool IsAlive,
+    int? Age) : IRequest<Result>;
