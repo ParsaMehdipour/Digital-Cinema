01776eefcc97769e892784d0bdd35c5ea80a0120
p.mehdipour <mehdipourparsa@gmail.com>
2024-04-05 15:56:39 +0330

    feat : add GetCast query and query handler

---
+++ 2.Application/Application/Casts/Queries/GetCast/GetCastQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Movies.DataTransferObjects;
+using FluentResults;
+using MediatR;
+
+namespace Application.Casts.Queries.GetCast;
+
+public record GetCastQuery(Guid Id) : IRequest<Result<CastDto>>;
+
