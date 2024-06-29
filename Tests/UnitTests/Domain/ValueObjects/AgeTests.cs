8f173a83a60f61c6d0c062e2a74d201a1578fcc6
Parsa <mehdipourparsa@gmail.com>
2024-06-29 17:21:42 -0400

    feat : add AgeTests and FirstNameTests (ValueObject tests)

---
+++ Tests/UnitTests/Domain/ValueObjects/AgeTests.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Shared;
+using Domain.ValueObjects;
+
+namespace UnitTests.Domain.ValueObjects;
+using static Xunit.Assert;
+
+public class AgeTests
+{
+    [Fact]
+    public void AgeInitialization_ReturnsSuccessResult_WhenPassedValueIsValid()
+    {
+        //Act
+        Result<Age> testAgeResult = Age.Create(23);
+
+        //Assert
+        True(testAgeResult.IsSuccess);
+        False(testAgeResult.IsFailure);
+    }
+
+
+    [Fact]
+    public void AgeInitialization_ReturnsFailedResult_WhenPassedValueIsNotValid()
+    {
+        //Act
+        Result<Age> testAgeResult = Age.Create(-1);
+
+        //Assert
+        True(testAgeResult.IsFailure);
+        False(testAgeResult.IsSuccess);
+    }
+}
---
+++ Tests/UnitTests/Domain/ValueObjects/FirstNameTest.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Shared;
+using Domain.ValueObjects;
+using static Xunit.Assert;
+
+namespace UnitTests.Domain.ValueObjects;
+
+public class FirstNameTest
+{
+    [Fact]
+    public void FirstNameInitialization_ReturnsSuccess_WhenPassedValueIsValid()
+    {
+        //Act
+        Result<FirstName> testFirstNameResult = FirstName.Create("Parsa");
+
+        //Assert
+        True(testFirstNameResult.IsSuccess);
+        False(testFirstNameResult.IsFailure);
+    }
+
+    [Fact]
+    public void FirstNameInitialization_ReturnsFailedResult_WhenPassedValueIsTooLong()
+    {
+        //Act
+        Result<FirstName> testFirstNameResult = FirstName.Create("kgosmntccgztecosktifnzrbhqtbjxmxylomrujltypxmbaoqauymvdwosmyvcmxzlhkebatrsnbhywxkotmxgjuxnr");
+
+        //Assert
+        True(testFirstNameResult.IsFailure);
+        False(testFirstNameResult.IsSuccess);
+    }
+}
